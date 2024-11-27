62d56a1400189a7ebfbaae7d1efc58b520eb4ffd
Diego Prado <diegoprd@gmail.com>
2024-11-27 18:07:55 +0100

    fixing lint 2

--- src/pages/TabataTimerPage.tsx
+++ src/pages/TabataTimerPage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import SecondsCountDownDisplayer from "../components/SecondsCountDownDisplayer";
-import { Link } from "react-router-dom";
 import HeaderWithBackButton from "../components/HeaderWithBackButton";
 
 interface TimerPhase {
